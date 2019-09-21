5c50af975cca156408d4da604e1b9baf3e04fba0
Maldi <themalekmalek@gmail.com>
2019-09-21 21:33:33 +0700

    try solve internal server error. in local not error

--- src/main/java/com/dokontekno/siantonx/controller/school/SchoolController.java
+++ src/main/java/com/dokontekno/siantonx/controller/school/SchoolController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping(value = SchoolController.PATH)
 public class SchoolController {
--- src/main/java/com/dokontekno/siantonx/controller/school/SchoolsController.java
+++ src/main/java/com/dokontekno/siantonx/controller/school/SchoolsController.java
@@ -4,13 +4,11 @@ import com.dokontekno.siantonx.controller.school.pojo.SchoolPojo;
 import com.dokontekno.siantonx.entity.school.School;
 import com.dokontekno.siantonx.service.school.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping(value = SchoolsController.PATH)
 public class SchoolsController {
