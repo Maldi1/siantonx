a712a3cac99181d577a94eae76ad25c5f4d0d5f0
Maldi <themalekmalek@gmail.com>
2019-10-08 00:53:55 +0700

    try solve internal server error. in local not error

--- src/main/java/com/dokontekno/siantonx/controller/school/view/SchoolsViewController.java
+++ src/main/java/com/dokontekno/siantonx/controller/school/view/SchoolsViewController.java
@@ -2,11 +2,13 @@ package com.dokontekno.siantonx.controller.school.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SchoolsViewController {
 
     @GetMapping(value = "/schools")
+    @ResponseBody
     public String getSchools(){
         return "schools";
     }
