eff22df73da349fe0ae9bc9010dbd56c0603894b
Maldi <themalekmalek@gmail.com>
2019-10-21 00:34:41 +0700

    new ui

--- src/main/java/com/dokontekno/siantonx/controller/school/view/SchoolViewController.java
+++ src/main/java/com/dokontekno/siantonx/controller/school/view/SchoolViewController.java
@@ -38,29 +38,9 @@ public class SchoolViewController {
         return "/school/schools";
     }
 
-    @GetMapping({"/schoolsamplex/{id}","/schoolsample1/{id}"})
-    public String schoolsample1(@PathVariable(required = true) UUID id, Model model) {
-        School school = schoolService.getSchool(id);
-        model.addAttribute("school",school);
-        model.addAttribute("sekolah", null);
-        model.addAttribute("companyname", "Someone & Co.");
-        model.addAttribute("values",100);
-        return "schoolsample1";
-    }
-
-    @GetMapping(value = "/schoolsample2")
-    public String schoolsample2(@RequestParam(name = "npsn", defaultValue = "%") String npsn,
-                          @RequestParam(name = "name", defaultValue = "%") String name,
-                          @RequestParam(name = "address", defaultValue = "%") String address,
-                          @RequestParam(name = "email", defaultValue = "%") String email,
-                          @RequestParam(name = "phoneNumber", defaultValue = "%") String phoneNumber,
-                          @RequestParam(name = "whatsappNumber", defaultValue = "%") String whatsappNumber,
-                          Model model){
-
-        List<School> schools = schoolService.getSchools(npsn, name, address, email, phoneNumber, whatsappNumber);
-        model.addAttribute("schools", schools);
-        return "schoolsample2";
-
+    @GetMapping("/index")
+    public String ind(){
+        return "index";
     }
 
 }
